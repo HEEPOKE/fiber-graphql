2fae350f92fbaa931f4146b73f910d2030cd97d3
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2024-03-29 11:29:15 +0700

    feat: update :money_with_wings:

--- internals/domains/graph/generated.go
+++ internals/domains/graph/generated.go
@@ -12,7 +12,7 @@ import (
 
 	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/graphql/introspection"
-	"github.com/HEEPOKE/fiber-graphql/graph/model"
+	"github.com/HEEPOKE/fiber-graphql/internals/domains/graph/model"
 	gqlparser "github.com/vektah/gqlparser/v2"
 	"github.com/vektah/gqlparser/v2/ast"
 )
--- pkg/constants/message_constants.go
+++ pkg/constants/message_constants.go
@@ -5,5 +5,5 @@ const (
 	CREATE_ACCESS_TOKEN_FAILED  = "create access token failed"
 	CREATE_REFRESH_TOKEN_FAILED = "create refresh token failed"
 	GET_ACCOUNT_DATA_FAILED     = "get account data failed"
-	PASSWORD_NOT_CORRECT        = "password not correct"
+	PW_NOT_CORRECT              = "password not correct"
 )
